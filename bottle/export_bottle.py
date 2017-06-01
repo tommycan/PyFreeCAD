e150c4062f3820939bd7b48b92cfc9e2c9894792
tommycan <tommy.c.andersson@gmail.com>
2017-06-01 13:25:57 +0200

    minor change in bottle

--- bottle/export_bottle.py
+++ bottle/export_bottle.py
@@ -1,8 +1,10 @@
 import Part, FreeCAD, math
 from FreeCAD import Base
 
-execfile("C:/Users/tommy/code/freecad/bottle.py") 
+#execfile("C:/Users/tommy/code/freecad/bottle.py") 
+execfile("bottle.py") 
 bottle = makeBottle(50,70,30)
 Part.show(bottle)
-bottle.exportStep("C:/Users/tommy/code/freecad/bottle.stp")
+#bottle.exportStep("C:/Users/tommy/code/freecad/bottle.stp")
+bottle.exportStep("bottle.stp")
  
